4b0673b196c8266047ad54fdfa924b6bf59fa9fc
aldrinjerry <jy8444@srmist.edu.in>
2026-08-20 16:28:11 +0530

    done

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 import sys
 import math 
 
-for line in sys.stdin:
-    line = line.strip()
+line = [line.strip() for line in sys.stdin if  line.strip()]
 
-    if not line:
-        continue
+x = [ float(v) for v in line[0].replace("INPUT","").split(",")]
+WEIGHTS = [float(v) for v in line[1].replace("WEIGHTS","").split(",")]
+BIAS = [float(v) for v in line[2].replace("BIAS","").split(",")]
+M,N = map(int,line[3].split()[2:])
+output =[]
+for i in range(M):
+    row = WEIGHTS[i *N:(i+1)*N]
+    z = sum(wi * xi for wi , xi in zip(row, x ))
+    z += BIAS[i]
+    result = 1/(1+math.exp(-z))
+    output.append(f"{result:.4f}")
 
-    x_csv,w_csv,b= line.split(";")
-
-    x = [float(v) for v in x_csv.split(",")]
-    w = [float(v) for v in w_csv.split(",")]
-    b = float(b)
-
-    z = sum(wi * xi for wi , xi in zip(w,x))+b
-
-    sigmoid = 1/(1+math.exp(-z))
-
-    print(f"{sigmoid:.4f}")
+print(",".join(output))
