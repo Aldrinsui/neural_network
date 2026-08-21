988c2d5675372800c0ed36ec34ea502d6f0422cb
aldrinjerry <jy8444@srmist.edu.in>
2026-08-21 23:20:47 +0530

    done

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 import sys
-import math 
+import math
 
 lines = [line.strip() for line in sys.stdin if line.strip()]
 
-pred = [float(v) for v  in lines[0].replace("PRED ","").split(",")]
-true =[float(v) for v in lines[1].replace("TRUE ","").split(",")]
+for line in lines:
+    x = float(line)
 
-squared_errors = []
+    s = 1 / (1 + math.exp(-x))
 
-for p,t in zip(pred,true):
-    error = p - t
-    squared_error = error * error
-    squared_errors.append(squared_error)
+    derivative = s * (1 - s)
 
-mse = sum(squared_errors)/len(squared_errors)
-
-print(f"{mse:.4f}")
+    print(f"{derivative:.4f}")
